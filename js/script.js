5e767d5801597cd4d7faf8182b9316e1ade5eff5
charliehaffner <143464756+charliehaffner@users.noreply.github.com>
2023-11-15 10:32:57 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+
+var map;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-  center: { lat: 41.598053655068185, lng: -87.56525859451041},
-  zoom: 12,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.598053655068185, lng: -87.56525859451041},
+    zoom: 12,
   });
   
   var maker = new google.maps.Marker({
-  position: { lat: 41.598053655068185, lng: -87.56525859451041},
-  map: map,
+    position: { lat: 41.598053655068185, lng: -87.56525859451041},
+    map: map,
   });
   
   var infowindow = new google.maps.InfoWindow({

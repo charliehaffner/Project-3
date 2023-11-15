411b1cadb1ed623b086adddc3094cac041325716
charliehaffner <143464756+charliehaffner@users.noreply.github.com>
2023-11-14 23:05:11 -0600

    Trying map

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+  center: { lat: 41.598053655068185, lng: -87.56525859451041},
+  zoom: 12,
+  });
+  
+  var maker = new google.maps.Marker({
+  position: {lat: 41.598053655068185, lng: -87.56525859451041},
+  zoom: 12,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content: "My Summer Internship",
+  });
+  infowindow.open(map, marker);
+  
+  const icons = {
+    gardencenter: {
+      icon: "../media/wilder.jpg",
+    },
+  };

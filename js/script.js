d03502146260ea709712f9b5c3930dee6361bfce
charliehaffner <143464756+charliehaffner@users.noreply.github.com>
2023-11-14 23:23:18 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,4 +24,13 @@ function initMap() {
     { position: { lat: 41.59, lng: -87.56 }, type: "gardencenter" },
   ];
   
+  for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
+}
+  
   google.maps.event.addDomListener(window, 'load', initMap);

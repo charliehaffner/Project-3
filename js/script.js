d226a7d5570a7fc2d26de56cabf47483defba289
charliehaffner <143464756+charliehaffner@users.noreply.github.com>
2023-11-15 19:59:51 -0600

    Changed normalize

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-
 var map;
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 41.598053655068185, lng: -87.56525859451041},
-    zoom: 12,
+    center: {lat: 41.8741, lng: -87.6285},
+    zoom: 15
   });
-  
-  var maker = new google.maps.Marker({
-    position: { lat: 41.598053655068185, lng: -87.56525859451041},
-    map: map,
+
+  var home = new google.maps.Marker({
+    position: { lat: 41.70839, lng: -87.65131},
+    map: map
   });
-  
-  var infowindow = new google.maps.InfoWindow({
-    content: "My Summer Internship",
+
+  var pratice = new google.maps.Marker({
+    position: { lat: 41.68480, lng: -87.72345 },
+    map: map
   });
-  infowindow.open(map, marker);
-  
-  const icons = {
-    Wilder Fields: {
-      icon: "../media/wilder.jpg",
-    },
-  };
-  
-  var features = [
-    { position: { lat: 41.59, lng: -87.56 }, type: "gardencenter" },
+
+  var distPoints = [
+    { lat: 41.8730, lng: -87.6279 },
+    { lat: 41.87299780452928, lng: -87.62762881329594 },
+    { lat: 41.869100, lng: -87.627030 },
+    { lat: 41.869280, lng: -87.625870 },
+    { lat: 41.868690, lng: -87.625380 },
   ];
-  
-  for (let i = 0; i < features.length; i++) {
-    const marker = new google.maps.Marker({
-      position: features[i].position,
-      icon: icons[features[i].type].icon,
-      map: map,
-    });
-  }
+
+  var dist = new google.maps.Polyline({
+    path: distPoints,
+    geodesic: true,
+    strokeColor: "#FE5F55",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  dist.setMap(map);
+
 }
-  
-  google.maps.event.addDomListener(window, 'load', initMap);
+
+window.initMap = initMap;
